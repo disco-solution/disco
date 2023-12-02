246fb9d3abfd86cf86fa6a33e6d43a121110b9f8
Wang Yucai <netowls-studio@outlook.com>
2023-12-02 23:04:08 +0800

    feat: 新增多语言资源文件。

---
+++ src/Disco.Common.MultilingualSupported/ExposedResourceManager.cs
@@ -0,0 +1,25 @@
+﻿// © 2023 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+namespace Disco.Globalization.Resources
+{
+    using System;
+    using System.Resources;
+
+    /// <summary> 提供了向外暴露的资源管理程序相关的方法。 </summary>
+    /// <seealso cref="ResourceManager" />
+    public static class ExposedResourceManager
+    {
+        /// <summary>
+        /// Disco 内置的资源管理程序。
+        /// <para> <see cref="ResourceManager" /> 类型的对象实例。 </para>
+        /// </summary>
+        /// <seealso cref="ResourceManager" />
+        /// <seealso cref="Lazy{T}" />
+        public static readonly Lazy<ResourceManager> BuiltInResourceManager =
+            new Lazy<ResourceManager>(
+                () => new ResourceManager(s_builtInResourceBaseName, typeof(ExposedResourceManager).Assembly),
+                true);
+
+        private const string s_builtInResourceBaseName = "Disco.Globalization.Resources.Strings";
+    }
+}
--- src/Disco.Common.MultilingualSupported/README.md
+++ src/Disco.Common.MultilingualSupported/README.md
@@ -4,15 +4,11 @@
 > 
 > Resource files required for internationalized multilingual support are provided.
 
-
-
 ## 支持的 .NET 版本 (Supported Microsoft .NET Versions)
 
 | .NET 平台<br/>.NET Platform | .NET 最低版本号<br/>.NET Minimum Version |
 | ------------------------- | ----------------------------------- |
 | .NET                      | 5                                   |
-| .NET Standard             | 1.0                                 |
-| .NET Framework            | 2.0                                 |
-| .NET Core                 | 3.1                                 |
-
-
+| .NET Standard             | 2.0                                 |
+| .NET Framework            | 4.5                                 |
+| .NET Core                 |                                     |
