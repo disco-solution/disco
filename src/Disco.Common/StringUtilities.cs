4f7533067c60fd1b4bc3171d6ecbd6457de3ca18
Wang Yucai <netowls-studio@outlook.com>
2023-12-05 22:08:09 +0800

    feat(#1): 新增 GetString 字符串工具方法。

--- src/Disco.Common/StringUtilities.cs
+++ src/Disco.Common/StringUtilities.cs
@@ -29,13 +29,41 @@ namespace Disco
             return s is null ? null : encoding.GetBytes(s);
         }
 
-        /// <summary> 使用 <see cref="UTF8Encoding"/> 获取字符串 <paramref name="s" /> 的字节数组。 </summary>
+        /// <summary> 使用 <see cref="UTF8Encoding" /> 获取字符串 <paramref name="s" /> 的字节数组。 </summary>
         /// <param name="s"> 字符串。 </param>
         /// <returns> 字节数组。 </returns>
         /// <seealso cref="Encoding" />
-        /// <seealso cref="UTF8Encoding"/>
-        /// <seealso cref="Encoding.UTF8"/>
+        /// <seealso cref="UTF8Encoding" />
+        /// <seealso cref="Encoding.UTF8" />
         public static byte[] GetBytes(string s) => GetBytes(s, Encoding.UTF8);
+
+        /// <summary> 使用指定的编码 <paramref name="encoding" /> 获取字符串。 </summary>
+        /// <param name="bytes"> 字节数组。 </param>
+        /// <param name="encoding">
+        /// 指定的编码。
+        /// <para> <see cref="Encoding" /> 类型的对象实例。 </para>
+        /// </param>
+        /// <returns> 字符串。 </returns>
+        /// <seealso cref="Encoding" />
+        /// <exception cref="ArgumentNullException"> 当 <paramref name="encoding" /> 等于 <see langword="null" /> 时，将引发此类型的异常。 </exception>
+        public static string GetString(byte[] bytes, Encoding encoding)
+        {
+            if (encoding is null)
+            {
+                throw new ArgumentNullException(nameof(encoding),
+                                                SR.Format("ArgumentNullException_exception_message_with_argument_name", nameof(encoding)));
+            }
+
+            return bytes is null || bytes.LongLength == 0 ? null : encoding.GetString(bytes);
+        }
+
+        /// <summary> 使用 <see cref="UTF8Encoding" /> 编码获取字符串。 </summary>
+        /// <param name="bytes"> 字节数组。 </param>
+        /// <returns> 字符串。 </returns>
+        /// <seealso cref="Encoding" />
+        /// <seealso cref="UTF8Encoding" />
+        /// <seealso cref="Encoding.UTF8" />
+        public static string GetString(byte[] bytes) => GetString(bytes, Encoding.UTF8);
 
         /// <summary>
         /// 当字符串 <paramref name="s" /> 满足 <paramref name="matches" /> 时，返回 <paramref name="ifMatches" /> 返回值，否则返回 <paramref
