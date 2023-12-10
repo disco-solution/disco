0833dfa9ff5fbbe2aebfa954d4b00f856dc31fea
Wang Yucai <netowls-studio@outlook.com>
2023-12-10 22:10:35 +0800

    feat(#1): 新增安全结果类型 Result。

---
+++ src/Disco.Common/IResult.cs
@@ -0,0 +1,29 @@
+﻿// © 2023 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+namespace Disco
+{
+    using System;
+
+    /// <summary> 定义了安全的方法结果的接口。 </summary>
+    public interface IResult
+    {
+        /// <summary> 执行过程中是否抛出了异常。 </summary>
+        /// <value> 获取一个 <see cref="bool" /> 类型值，用于表示执行过程中是否抛出了异常。 </value>
+        bool HasException { get; }
+
+        /// <summary> 方法执行中抛出的异常。 </summary>
+        /// <value> 获取 <see cref="Exception" /> 类型的对象实例，用于表示方法执行中抛出的异常。 </value>
+        /// <seealso cref="Exception" />
+        Exception ProcessingException { get; }
+    }
+
+    /// <summary> 定义了包含 <typeparamref name="TReturn" /> 类型结果值的安全方法结果的接口。 </summary>
+    /// <typeparam name="TReturn"> 方法结果类型。 </typeparam>
+    /// <seealso cref="IResult" />
+    public interface IResult<TReturn> : IResult
+    {
+        /// <summary> 实际的 <typeparamref name="TReturn" /> 类型的方法返回值。 </summary>
+        /// <value> 获取 <typeparamref name="TReturn" /> 类型的对象实例或值，用于表示实际的 <typeparamref name="TReturn" /> 类型的方法返回值。 </value>
+        TReturn Value { get; }
+    }
+}
---
+++ src/Disco.Common/Result.cs
@@ -0,0 +1,46 @@
+﻿// © 2023 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+namespace Disco
+{
+    using System;
+
+    /// <summary> 提供了安全的方法结果相关的方法。 </summary>
+    /// <seealso cref="IResult" />
+    public class Result : IResult
+    {
+        /// <summary> 初始化 <see cref="Result" /> 的新实例。 </summary>
+        public Result() : this(null)
+        {
+        }
+
+        /// <summary> 初始化 <see cref="Result" /> 的新实例。 </summary>
+        /// <param name="exception"> 方法执行过程中的异常。 </param>
+        public Result(Exception exception)
+        {
+            ProcessingException = exception;
+        }
+
+        /// <inheritdoc />
+        public virtual bool HasException
+        {
+            get { return !(ProcessingException is null); }
+        }
+
+        /// <inheritdoc />
+        public virtual Exception ProcessingException { get; }
+
+        /// <summary> 创建一个执行成功的方法结果。 </summary>
+        /// <returns> 实现了 <see cref="IResult" /> 类型接口的对象实例。 </returns>
+        /// <seealso cref="IResult" />
+        public static IResult Complete() => new Result();
+
+        /// <summary> 创建一个异常的方法结果。 </summary>
+        /// <param name="exception">
+        /// 方法执行过程中的异常。
+        /// <para> <see cref="Exception" /> 类型的对象实例。 </para>
+        /// </param>
+        /// <returns> 实现了 <see cref="IResult" /> 类型接口的对象实例。 </returns>
+        /// <seealso cref="IResult" />
+        public static IResult Error(Exception exception) => new Result(exception);
+    }
+}
