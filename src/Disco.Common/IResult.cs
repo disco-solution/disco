f2bc46ceb720333f6a557ea434800c7eaa730b5d
Wang Yucai <netowls-studio@outlook.com>
2023-12-10 22:14:26 +0800

    style: 拆分了 IResult 泛型结果到新的代码文件。

--- src/Disco.Common/IResult.cs
+++ src/Disco.Common/IResult.cs
@@ -16,14 +16,4 @@ namespace Disco
         /// <seealso cref="Exception" />
         Exception ProcessingException { get; }
     }
-
-    /// <summary> 定义了包含 <typeparamref name="TReturn" /> 类型结果值的安全方法结果的接口。 </summary>
-    /// <typeparam name="TReturn"> 方法结果类型。 </typeparam>
-    /// <seealso cref="IResult" />
-    public interface IResult<TReturn> : IResult
-    {
-        /// <summary> 实际的 <typeparamref name="TReturn" /> 类型的方法返回值。 </summary>
-        /// <value> 获取 <typeparamref name="TReturn" /> 类型的对象实例或值，用于表示实际的 <typeparamref name="TReturn" /> 类型的方法返回值。 </value>
-        TReturn Value { get; }
-    }
 }
